97dec94aa57f8bf2d05c2a98661adbef7a8c431c
Rodrigo <rodrigo.hammes7@gmail.com>
2021-04-13 23:54:56 -0300

    inicia login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Flex } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-   <div>
-     <h1>Dashgo</h1>
-   </div>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+      >
+        asf
+      </Flex>
+    </Flex>
   )
 }
